451d87f423ebec07f3a8e0b33cffcd38329c3b61
Janaina Moreira-Kanaley <J.M.Moreira-Kanaley@student.tudelft.nl>
2023-05-15 12:29:30 +0200

    Add counter for positive and negative predictions

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 CORS(app)
 swagger = Swagger(app)
 
+countPosPredictions = 0
+countNegPredictions = 0
+
 @app.route('/predict', methods=['POST'])
 def predict():
 	"""
@@ -36,14 +39,36 @@ def predict():
 	review = request.get_json().get('review')
 
 	# Make a prediction on the message
-	prediction = model_predict(review)
+	prediction = int(model_predict(review))
+
+	if prediction == 0:
+	    global countNegPredictions
+        countNegPredictions += 1
+    else:
+	    global countPosPredictions
+        countPosPredictions += 1
 
 	response = flask.jsonify({
 		"review": review,
-		"prediction": int(prediction),
+		"prediction": prediction,
 	})
 
 	return response
+
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    global countNegPredictions, countPosPredictions
+    m = "# HELP my_random This is just a random 'gauge' for illustration.\n"
+    m+= "# TYPE my_random gauge\n"
+    m+= "my_random " + str(random()) + "\n\n"
+
+    m+= "# HELP num_requests The number of requests that have been served, by page.\n"
+    m+= "# TYPE num_requests counter\n"
+    m+= "num_pos_requests{{page=\"predict\"}} {}\n".format(countIdx)
+    m+= "num_neg_requests{{page=\"predict\"}} {}\n".format(countSub)
+    m+= "num_requests{{page=\"predict\"}} {}\n".format(countPosPredictions + countNegPredictions)
+
+    return Response(m, mimetype="text/plain")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
